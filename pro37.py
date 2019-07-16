443ebeb855a6f00a0bdc212f201688529a5d6397
soundar001 <soundar813@gmail.com>
2019-07-16 12:38:00 +0530

    Update pro37.py

--- pro37.py
+++ pro37.py
@@ -1,12 +1,12 @@
 import sys,string
-n = int(input())
+n1 = int(input())
 L = [ int(x) for x in input().split()]
-n = len(L)
-if n==1 :
+n1 = len(L)
+if n1==1 :
     print(1)
     sys.exit()
 cnt = 0
-for i in range(1,n-1) :
+for i in range(1,n1-1) :
     if ((L[i] > L[i-1]) and (L[i] > L[i+1])) or ((L[i] < L[i-1]) and (L[i] < L[i+1])):
         cnt += 1
 print(cnt)
