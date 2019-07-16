a87466b1f72a2130eaafd5237ca30ab97502e034
soundar001 <soundar813@gmail.com>
2019-07-16 12:31:57 +0530

    Update pro35.py

--- pro35.py
+++ pro35.py
@@ -9,20 +9,18 @@ s = input()
 n = len(s)
 dic1 = cfreq(s)
 Lk = list(dic1.keys())
-#print(dic1,Lk)
-
 for j in range(n-2,-1,-1) :
-    #print('len = ', j+1)
+  
     for c in Lk :
         k = 0
         for i in range(0,n-j) :
             li, ri = i,j+i
             s2 = s[li:ri + 1]
-            #print(c,s2)
+           
             if c in s2 :
                 k += 1
         if k == n-j :
-            #print('c,k',c,k)
+          
             c2 = c
             k2 = k
             len2 = j+1
