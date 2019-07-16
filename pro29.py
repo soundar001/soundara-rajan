71352ed4220dc5a0b76517f7de1a6ca6da1048c0
soundar001 <soundar813@gmail.com>
2019-07-16 11:59:48 +0530

    Create pro29.py

---
+++ pro29.py
@@ -0,0 +1,25 @@
+import sys, string, math
+import sys,string
+def dgtSum(n1) :
+    sum1 = 0
+    while n1 :
+        sum1 += n%10
+        n1 //= 10
+    return sum1
+n1 = int(input())
+L = []
+cnt = 0
+if n1 <= 5000 :
+    for i in range(1,n1) :
+        if i + dgtSum(i) == n1 :
+            cnt += 1
+            L.append(i)
+    print(cnt)
+    print(*L)
+else :
+    for i in range(n1-1000,n1) :
+        if i + dgtSum(i) == n1 :
+            cnt += 1
+            L.append(i)
+    print(cnt)
+    print(*L)
