916bf68523e39e34f3e69a7ad52500a0ac60a00e
soundar001 <soundar813@gmail.com>
2019-07-16 12:19:51 +0530

    Update pro34.py

--- pro34.py
+++ pro34.py
@@ -1,35 +1,35 @@
 import sys, string, math
-def maxAltSeq(s) :
-    c = s[0]
+def maxAltSeq(s1) :
+    c = s1[0]
     L = [c]
-    n = len(s)
+    n = len(s1)
     maxL = [c]
     for i in range(1,n) :
-        if s[i] != c :
-            c = s[i]
+        if s1[i] != c :
+            c = s1[i]
             L.append(c)
             if len(L) > len(maxL) :
                 maxL = L[:]
         else :
-            c = s[i]
+            c = s1[i]
             L = [c]
-        s1 = ''.join(maxL)
+        s2 = ''.join(maxL)
     
-    s2 = ''.join(maxL)
-    i1 = s.index(s2)
-    i2 = i1 + len(s2) - 1
-    return [i1,i2,s2]
+    s3 = ''.join(maxL)
+    i1 = s1.index(s3)
+    i2 = i1 + len(s3) - 1
+    return [i1,i2,s3]
 
-def cherry2(s) :
-    L = list(s)
-    n = len(s)
-    i1,i2,x = maxAltSeq(s)
+def cherry2(s1) :
+    L = list(s1)
+    n = len(s1)
+    i1,i2,x = maxAltSeq(s1)
 
     sum1 = 0
     if i1 > 0 :
-        c = s[i1]
+        c = s1[i1]
         for k1 in range(i1-1,-1,-1) :
-            if s[k1] == c:
+            if s1[k1] == c:
                 if c == 'A':
                     L[k1] = 'B'
                     sum1 += 5
@@ -39,11 +39,11 @@ def cherry2(s) :
                     sum1 += 3
                     c = 'A'
             else:
-                c = s[k1]
+                c = s1[k1]
     if i2 < n-1 :
-        c = s[i2]
+        c = s1[i2]
         for k1 in range(i2+1,n) :
-            if s[k1] == c :
+            if s1[k1] == c :
                 if c == 'A' :
                     L[k1] = 'B'
                     sum1 += 5
@@ -53,8 +53,8 @@ def cherry2(s) :
                     sum1 += 3
                     c = 'A'
             else :
-                c = s[k1]
-        s3 = ''.join(L)
+                c = s1[k1]
+        s4 = ''.join(L)
        
     return sum1
 
@@ -63,8 +63,8 @@ n,k = int(n),int(k)
 sum1 = 0
 L = [ input() for i in range(0,n)]
 for i in range(0,n) :
-    s = L[i]
-    s1 = s.replace('R','A')
-    s2 = s1.replace('G','B')
-    sum1 += cherry2(s2)
+    s1 = L[i]
+    s2 = s1.replace('R','A')
+    s3 = s2.replace('G','B')
+    sum1 += cherry2(s3)
 print(sum1)
