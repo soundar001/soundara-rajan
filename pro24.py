f51cbecf746d6c57d3332ac213db4075f4401afd
soundar001 <soundar813@gmail.com>
2019-07-16 11:46:31 +0530

    Update pro24.py

--- pro24.py
+++ pro24.py
@@ -6,7 +6,7 @@ for i in range(0,k1) :
     s1 = bin(i)[2:]
     j1 = len(s1)
     if j1 < n1 :
-        s1 = '0' * (n1-j) + s1
+        s1 = '0' * (n1-j1) + s1
     L.append(s1)
 for i in range(0,len(L)) :
     print(L[i])
