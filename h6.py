05e07195a921c184a604c7eebf8c0d5f3bddeb6b
soundar001 <soundar813@gmail.com>
2019-07-16 11:21:27 +0530

    Create h6.py

---
+++ h6.py
@@ -0,0 +1,10 @@
+import sys, string, math
+n1 = int(input())
+L1 = list(map(int,input().split()))
+flags = 1
+for i in range(len(L1)) :
+    if L1.count(L1[i]) > 1 :
+        print(L1[i])
+        flags = 0
+        break
+if flag == 1 : print('unique')
