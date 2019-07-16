bb391bcf5e4748f4a3d0ae54be144c44f292eac7
soundar001 <soundar813@gmail.com>
2019-07-16 12:03:57 +0530

    Update pro30.py

--- pro30.py
+++ pro30.py
@@ -22,19 +22,16 @@ if n2%8 == 0 :
     sys.exit()
 s2 = s[:-1]
 n2 = int(s2)
-#print(s2,n2)
 if n2%8 == 0 :
     print('yes')
     sys.exit()
 s2 = s[:-2] + s[-1]
 n2 = int(s2)
-#print(s2,n2)
 if n2%8 == 0 :
     print('yes')
     sys.exit()
 s2 = s[:-3] + s[-2:]
 n2 = int(s2)
-#print(s2,n2)
 if n2%8 == 0 :
     print('yes')
     sys.exit()
