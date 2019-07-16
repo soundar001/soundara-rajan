265a91005be1fbdd3594a9ad279da48742851df9
soundar001 <soundar813@gmail.com>
2019-07-16 14:23:20 +0530

    Update pro51.py

--- pro51.py
+++ pro51.py
@@ -1,14 +1,23 @@
-import sys, string, math
-s1 = input()
-s2 = input()
-s3 = string.ascii_lowercase
-L = []
+def ch(l):
+        c=1
+        
+        for x in range(0,len(l)-1):
+                if l[x]!=l[x+1]:
+                        c=c+1
+                else:
+                    break
+        return c
+num=int(input())
+lin=list(map(int,input().split()))
 
-for i in range(0, len(s1)) :
-    k = ord(s1[i]) + s3.index(s2[i]) + 1
-    if k > ord('a')+25 :
-        k -= 26
-    
-    L.append(chr(k))
-s4 = ''.join(L)
-print(s4)
+for x in range(0,len(lin)):
+        if lin[x]>0:
+                lin[x]=1
+        else:
+             lin[x]=0
+A=""
+
+for x in range(0,len(lin)):
+        B=lin[x::]
+        A=A+str(ch(B))+" "
+print(A.strip())
